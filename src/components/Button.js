23921e4b9587b05e111238c732f0403b76236261
Abayomi <omosehin14@gmail.com>
2019-10-19 15:45:04 +0100

    implement header,button and card component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({Children,btnType,myBtnClass}) => {
     return (
         <div>
-            
+            <button className = {`btn ${btnType} ${myBtnClass}`}>
+                {Children}
+            </button>
         </div>
     )
 }
