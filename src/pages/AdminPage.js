cb4b283c5e52e42f0ab24d55ea48fabf4ffe4435
dave-io <david.chrisabbey@gmail.com>
2019-10-22 17:28:11 +0100

    style dashboard table

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from "../components/Nav";
-import JobsTable from '../components/JobsTable';
+import Dashboard from '../components/Dashboard';
 import Header from "../components/Header"
 
 const AdminPage = () => {
@@ -9,7 +9,7 @@ const AdminPage = () => {
             <Nav ViewJobs="All Jobs" AddJobs ="Add Jobs"  />
             <Header className="adminHeader" headerText="signHeaderText" HeaderText__first= "Admin" SubHeaderText="Job offers available"   />
 
-            <JobsTable />
+            <Dashboard />
         </div>
     )
 }
