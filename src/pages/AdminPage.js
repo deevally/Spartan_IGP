f60439d5345c808e48c4e773a01258d9ec44b922
Abayomi <omosehin14@gmail.com>
2019-10-22 20:55:11 +0100

    Added footer admin page

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Nav from "../components/Nav";
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const AdminPage = () => {
     return (
         <div>
             <Nav ViewJobs="All Jobs" AddJobs ="Add Jobs"  />
             <Header className="adminHeader" headerText="signHeaderText" HeaderText__first= "Admin" SubHeaderText="Job offers available"   />
-
+            <Footer/>
         </div>
     )
 }
