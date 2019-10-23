bca80f05e5e750e3e41f73e3b422ae2a21b2530b
dave-io <david.chrisabbey@gmail.com>
2019-10-23 20:31:16 +0100

    added delete functionality and sidebar feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,52 +1,101 @@
 import React from 'react';
 import '../css/JobsTable.css';
+import {BaseUrl} from '../utils/baseUrl';
 
 class JobsTable extends React.Component {
   constructor(props) {
      super(props)
      this.state = {
-        jobListing: [
-           { id: 1, date: 'mm/dd/yr', company: 'VGG', type: 'remote', location: 19, status: 'active', action: 'delete'}
-           
-        ]
+        jobListing: [],
+        jobHeader:{
+           no: 'No',
+           jobTitle: 'Job Title',
+           jobType: 'Job Type',
+           salary: 'Salary',
+           action: 'Delete'
+        },
      }
   }
 
+
+  componentDidMount() {
+     let url =`${BaseUrl}/jobs`
+     console.log(url)
+     fetch(url)
+     .then(response => {
+        return response.json();
+     })
+     .then(data => {
+       console.log(data)
+
+        this.setState({jobListing:data.docs})
+     })
+  }
+
+
+  handleDelete=(id)=>{
+   let url =`${BaseUrl}/job/${id}`
+   fetch(url, {method: 'DELETE'})
+   .then(() => {
+       console.log('removed');
+    })
+    .catch(err => {
+      console.error(err)
+  })
+  this.DeleteId(id)
+  }
+
+   DeleteId =(id)=>{
+      const DeleteID = this.state.jobListing.filter(job=>job._id !==id)
+      this.setState({
+         jobListing:DeleteID
+      })
+   }
+
   renderTableHeader() {
-     let header = Object.keys(this.state.jobListing[0])
+     let header = Object.values(this.state.jobHeader)
      return header.map((key, index) => {
         return <th key={index}>{key.toUpperCase()}</th>
      })
   }
 
   renderTableData() {
-     return this.state.jobListing.map((jobs, index) => {
-        const { id, date, company, type, location, status, action} = jobs 
+     
+     let i = 1;
+     return this.state.jobListing.map(job => {
+        const { _id, JobTitle, JobType, salary} = job 
         return (
-           <tr key={id}>
-              <td>{id}</td>
-              <td>{date}</td>
-              <td>{company}</td>
-              <td>{type}</td>
-              <td>{location}</td>
-              <td>{status}</td>
-              <td>{action}</td>
+           <tr key={_id}>
+              <td>{i++}</td>
+              <td>{JobTitle}</td>
+              <td>{JobType}</td>
+              <td>{salary}</td>
+              <td><button onClick={()=>this.handleDelete(_id)}>Delete</button></td>             
            </tr>
         )
      })
   }
 
+
+
   render() {
      return (
-        <div>
+        <React.Fragment>
+        <div className='adminSidebar'>
+           <a className='p-4 pt-5 text-white' href='#'>Home</a>
+           <a className='p-4 pt-2 text-white' href='/'>Logout</a>
+           <a className='p-4 pb-5 text-white' href='/'>Change Password</a>
+        </div>
+        <div className='tableBorder'>
            <h2 id='title'>Current Job Listing</h2>
-           <table id='jobListing'>
+           <table id='jobListing' className='mx-auto' >
               <tbody>
                  <tr>{this.renderTableHeader()}</tr>
                  {this.renderTableData()}
               </tbody>
            </table>
         </div>
+        </React.Fragment>
      )
   }
 }
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -7,7 +7,7 @@ const AdminPage = () => {
     return (
         <div>
             <Nav ViewJobs="All Jobs" AddJobs ="Add Jobs"  />
-            <Header className="adminHeader" headerText="signHeaderText" HeaderText__first= "Admin" SubHeaderText="Job offers available"   />
+            <Header className="adminHeader" headerText="signHeaderText" HeaderText__first= "Admin" />
 
             <Dashboard />
         </div>
