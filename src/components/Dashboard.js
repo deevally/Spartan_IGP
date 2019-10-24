6fe4cf29526a58782fd5cd7af6fe44c37fff63f4
dave-io <david.chrisabbey@gmail.com>
2019-10-24 20:11:08 +0100

    dashboard styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/JobsTable.css';
 import {BaseUrl} from '../utils/baseUrl';
+import Nav from './Nav';
 
 class JobsTable extends React.Component {
   constructor(props) {
@@ -11,8 +12,9 @@ class JobsTable extends React.Component {
            no: 'No',
            jobTitle: 'Job Title',
            jobType: 'Job Type',
+           location: 'Location',
            salary: 'Salary',
-           action: 'Delete'
+           action: 'Action'
         },
      }
   }
@@ -28,12 +30,12 @@ class JobsTable extends React.Component {
      .then(data => {
        console.log(data)
 
-        this.setState({jobListing:data.docs})
+        this.setState({jobListing: data.docs})
      })
   }
 
 
-  handleDelete=(id)=>{
+  handleDelete=(id) => {
    let url =`${BaseUrl}/job/${id}`
    fetch(url, {method: 'DELETE'})
    .then(() => {
@@ -45,8 +47,8 @@ class JobsTable extends React.Component {
   this.DeleteId(id)
   }
 
-   DeleteId =(id)=>{
-      const DeleteID = this.state.jobListing.filter(job=>job._id !==id)
+   DeleteId = id => {
+      const DeleteID = this.state.jobListing.filter(job => job._id !== id)
       this.setState({
          jobListing:DeleteID
       })
@@ -63,12 +65,13 @@ class JobsTable extends React.Component {
      
      let i = 1;
      return this.state.jobListing.map(job => {
-        const { _id, JobTitle, JobType, salary} = job 
+        const {_id, JobTitle, JobType, location, salary} = job
         return (
            <tr key={_id}>
               <td>{i++}</td>
               <td>{JobTitle}</td>
               <td>{JobType}</td>
+              <td>{location}</td>
               <td>{salary}</td>
               <td><button onClick={()=>this.handleDelete(_id)}>Delete</button></td>             
            </tr>
@@ -82,10 +85,12 @@ class JobsTable extends React.Component {
      return (
         <React.Fragment>
         <div className='adminSidebar'>
-           <a className='p-4 pt-5 text-white' href='#'>Home</a>
-           <a className='p-4 pt-2 text-white' href='/'>Logout</a>
-           <a className='p-4 pb-5 text-white' href='/'>Change Password</a>
+            <a className='p-4 pt-5 text-white' href={'/'}><i className="fas fa-home"></i></a>
+            <a className='p-4 pt-5 text-white' href={'/Add_Jobs'}><i className="fas fa-paste"></i></a>
+            <a className='p-4 pt-2 text-white' href='/'><i className="fas fa-power-off"></i></a>
+            <a className='p-4 pb-5 text-white' href='/'><i className="fas fa-key"></i></a>
         </div>
+         <Nav />
         <div className='tableBorder'>
            <h2 id='title'>Current Job Listing</h2>
            <table id='jobListing' className='mx-auto' >
