57c8a80ac9b41164f7945e983f86631d9c70ff76
dave-io <david.chrisabbey@gmail.com>
2019-10-25 12:36:38 +0100

    some extra styling

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Dashboard from '../components/Dashboard';
 import Nav from '../components/Nav';
-import Header from "../components/Header"
 
 const AdminPage = () => {
     return (
         <div>
             <Nav />
-             {/* <Header className="adminHeader"   */}
-                 {/* headerText="signHeaderText"   */}
-                 {/* HeaderText__first= "Admin"   */}
-            />
             <Dashboard />
         </div>
     )
