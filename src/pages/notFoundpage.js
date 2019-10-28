7c75f0c199b2af1d5fda9df9dbdbe5eb8d5ee813
Funmilayo Anihundidun <anihundidunfunmilayo@gmail.com>
2019-10-28 12:18:51 +0100

    notfound pagesones

--- src/pages/notFoundpage.js
+++ src/pages/notFoundpage.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
-import '../css/JobsTable.css'
+import PageNotFound from "../components/NotFound";
 
 const NotFound = () => {
   return (
-      <div>
-    <div className="container">
-      <div className="row">
-        <div className="col-12 notFound">
-            <h1 className='font-weight-bolder'>404</h1>
-          <h4>The page you are looking for does not exist</h4>
-          <Link to="/">Go to Home</Link>
-        </div>
-      </div>
+    <div>
+      <Nav />
+      <PageNotFound />
     </div>
-      </div>
   );
 };
 
