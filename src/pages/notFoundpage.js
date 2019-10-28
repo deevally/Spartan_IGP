45c9b95d690f1434fa7c1240e3ba24cd965200c9
geraldanosike <geraldanosike.javhse@gmail.com>
2019-10-28 11:58:13 +0100

    pagenotfound

--- src/pages/notFoundpage.js
+++ src/pages/notFoundpage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Nav from '../components/Nav';
-import PageNotFound from '../components/NotFound'
+import React from "react";
+import Nav from "../components/Nav";
+import PageNotFound from "../components/NotFound";
 
 const NotFound = () => {
-    return ( 
-        <div>
-<Nav/>
-<PageNotFound/>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <Nav />
+      <PageNotFound />
+    </div>
+  );
+};
+
 export default NotFound;
