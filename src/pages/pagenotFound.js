b9aff78e96bf91c207d36257c3648918a605df17
Abayomi <omosehin14@gmail.com>
2019-11-19 08:11:54 +0100

    resolve develop

---
+++ src/pages/pagenotFound.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../components/Nav";
+import PageNotFound from "../components/NotFound";
+
+const NotFound = () => {
+  return (
+    <div>
+            
+      <Nav />
+            
+      <PageNotFound />
+          
+    </div>
+  );
+};
+
+export default NotFound;
